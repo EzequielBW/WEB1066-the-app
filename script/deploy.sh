58e20405571f8d107eb4bb63df43b8a9e3fa45e3
EzequielBW <ezebw2@gmail.com>
2021-02-02 07:05:59 -0300

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,5 +2,5 @@
 set -e
 
 bash -c "echo \"${DOCKER_PASSWORD}\" | docker login --username \"${DOCKER_LOGIN}\" --password-stdin"
-docker tag zutherb/monolithic-shop:latest $DOCKER_LOGIN/monolithic-shop:latest
+docker tag zutherb/monolithic-shop:latest $DOCKER_LOGIN/web1066:latest
 docker push $DOCKER_LOGIN/web1066:latest
